b5506d414d6b93c5c21f4ae50f4139f7071426bf
Ganesh B <ganeshsurfs@gmail.com>
2020-01-06 13:06:14 +0530

    Adding single line diction creation using for loop expression - k:v both are same list items

---
+++ 3 - Types/3.3 - InbuiltTypes-DictionarySetArray/14-dictions-creating-dictions-using-for-loop-expressions.py
@@ -0,0 +1,6 @@
+# Create a diction from a list which has 
+#       items as keys and values
+# Single line expression using for loop single line expression
+
+dictionList = ["a", "b", "c", "d", "e"]
+print({k:k for k in dictionList})
