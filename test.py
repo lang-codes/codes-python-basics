b5e8488800d8dd9a26052eea980ddf6a1c5e212c
Ganesh B <ganeshsurfs@gmail.com>
2020-01-08 16:38:43 +0530

    Adding links for Python demo codes

--- test.py
+++ test.py
@@ -23,5 +23,5 @@ with open("./README.md", mode="w", encoding="utf8") as f:
 
 * Topic 
 
-        """.format(fl.lstrip('.')))
+""".format(fl.lstrip('.')))
         f.write("""[{0}](https://github.com/python-demo-codes/basics/blob/master{0})""".format(fl.lstrip('.')) + '\n\r')
